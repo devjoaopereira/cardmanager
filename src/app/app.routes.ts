c9962167562d94dd406a1618de71835cdc0873c2
João Pereira <dev.joaopereira@gmail.com>
2024-05-16 12:00:52 -0300

    Criando o componente deck e sua rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { DeckComponent } from './components/deck/deck.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'mydeck',
+        component: DeckComponent
     }
 ];
