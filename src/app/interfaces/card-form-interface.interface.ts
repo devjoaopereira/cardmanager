f8839ab939e86fc629bc6e64055e5fba5743e66b
João Pereira <dev.joaopereira@gmail.com>
2024-05-16 02:26:07 -0300

    Criando interfaces CardFormInterface e SetInterface

---
+++ src/app/interfaces/card-form-interface.interface.ts
@@ -0,0 +1,4 @@
+export interface CardFormInterface {
+    block: string;
+    name?: string;
+}
---
+++ src/app/interfaces/set-interface.interface.ts
@@ -0,0 +1,6 @@
+export interface SetInterface {
+    block: string;
+    code: string;
+    name: string;
+    releaseDate: string;
+}
